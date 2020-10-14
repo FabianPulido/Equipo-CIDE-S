b81219bb9dcb3d11314437fdb791926c0ce91962
Michael-Vargas08 <piracocamaikol@gmail.com>
2020-10-14 12:24:34 -0500

    Michael
    realice el dasboard de las sucursales enlazando el mismo css de las tiendas

---
+++ PHP/DashboardS.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/diseñado.css">
+    <title>DashT</title>
+</head>
+<body>
+       <!--  navbar principal  -->
+       <div class="navbar">
+        
+        <a href="" class="boton">Usuarios</a>
+
+        <a href="" class="enlace">Nueva Tienda</a>
+
+        <a href="" class="enlace2">Administrar Tiendas</a>
+        </div>
+    <!-- ///////// -->   
+</body>
+</html>
