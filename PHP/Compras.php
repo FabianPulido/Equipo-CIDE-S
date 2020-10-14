8759987d09b74f499852fec69341cf52af291432
Michael-Vargas08 <piracocamaikol@gmail.com>
2020-10-14 12:08:23 -0500

    Michael
    volver a realizar los formularios debido problemas internos

---
+++ PHP/Compras.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/diseñoF.css">
+    
+    <title>Compra</title>
+</head>
+<body>
+    <!-- Fomulario de compra -->
+        <div class="formulario">
+                <h1>
+                    Compras                
+                </h1>
+            <form action="">
+                <table>
+                    <tr>
+                        <td>
+                            Id Compra
+                        </td>
+                        <td>
+                            <input type="text" name="Id" id="">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Fecha
+                        </td>
+                        <td>
+                            <input type="date" name="Fecha" id="">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Id Producto
+                        </td>
+                        <td>
+                            <input type="text" name="Id_Producto" id="">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Cantidad
+                        </td>
+                        <td>
+                            <input type="text" name="Cantidad" id="">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Valor
+                        </td>
+                        <td>
+                            <input type="text" name="Valor" id="">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                           <input type="submit" value="Enviar" class="boton">
+                        </td>
+                    </tr>
+                </table>
+
+            </form>
+
+        </div>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////// -->         
+
+
+
+</body>
+</html>
--- PHP/DashboardAdmin.php
+++ PHP/DashboardAdmin.php
@@ -1,3 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/diseñado.css">
+    <title>DashT</title>
+</head>
+<body>
+       <!--  navbar principal  -->
+       <div class="navbar">
+        
+        <a href="" class="boton">Usuarios</a>
 
-?>
+        <a href="" class="enlace">Nueva Sucursal</a>
+
+        <a href="" class="enlace2">Administrar Sucursales</a>
+        </div>
+    <!-- ///////// -->   
+</body>
+</html>
