10b421e92ac3ac54ccef0e1e4b4c8d8a221de240
Michael-Vargas08 <piracocamaikol@gmail.com>
2020-10-14 14:50:59 -0500

    actualizacion
    registra datos de las sucursales que son creadas

--- PHP/DashboardAdmin.php
+++ PHP/DashboardAdmin.php
@@ -46,7 +46,7 @@
     <!-- Formulario de Login -->
     <section class="form-register" id="form">
         
-        <form action="" method="post">
+        <form action="FormAdmin/RegistrarSS.php" method="post">
 
             <h4>Registrar Sede/Sucursal</h4>
 
@@ -54,10 +54,10 @@
             <input class="controls" type="text" name="NombreSede" placeholder="Ingrese Nombre Sede/Sucursal">
     
             <!-- Input EmailSede/Sucursal -->
-            <input class="controls" type="email" name="EmailSede" placeholder="Ingrese Correo Sede/Sucursal">
+            <input class="controls" type="text" name="EmailSede" placeholder="Ingrese Direccion Sede/Sucursal">
     
             <!-- Input PasswordSede/Sucursal -->
-            <input class="controls" type="password" name="PasswordSede" placeholder="Ingrese Contraseña Sede/Sucursal">
+            <input class="controls" type="text" name="PasswordSede" placeholder="Ingrese Telefono Sede/Sucursal">
 
             <input class="botons" type="submit" value="Registrar">
 
