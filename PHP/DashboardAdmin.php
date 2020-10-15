8db2432a04a77938c2ba19fc9c006e27860d99f1
FabianPulido <oscarpulido758@gmail.com>
2020-10-14 20:55:42 -0500

    hj

--- PHP/DashboardAdmin.php
+++ PHP/DashboardAdmin.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   
     <!-- Link CSS -->
-    <link rel="stylesheet" href="../CSS/Dashboar.css">
+    <link rel="stylesheet" href="../CSS/Dashboarr.css">
 
     <title>Dashboard Admin</title>
 
@@ -64,12 +64,12 @@
     </section>
 
     <!-- Tabla para mostrar las sucursales -->
-    <div class="show-SS" style="height:100%;width:100%;">
+    <div class="show-SS" style="height:450px;width:100%;overflow:scroll;">
        
-       <table class="table table-dark">
-     <thead>
-     <th scope="col"><input type="text"></th>
-       <tr>
+      
+    <table class="table table-dark" >
+  <thead>
+  <th scope="col"><input type="text"></th>
     <tr>
       <th scope="col">ID</th>
       <th scope="col">Nombre</th>
@@ -106,7 +106,7 @@
         {$row['Telefono_sucursal']}</td>
         <td>
         <button type='submit' id='botoneditar'>Editar</button>
-        <button type='submit' id='botonborrar'>Borrar</button>
+        <a href='eliminar.php?=echo Id_sucursal{row['Id_sucursal']' id='botonborrar'>Borrar</a>
         </td>
         </tr>";
         }
@@ -117,6 +117,11 @@
 </table>
   
     </div>
+    <div class="contenedor_imagen">
+    <div class="imagensucur">
+        <img src="../ASSETS/imagensucursales.jpg" width=100% height=100%;>
+      </div>
+      </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
